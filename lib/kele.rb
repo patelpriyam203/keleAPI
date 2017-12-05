f4dc60989414c27afab01e7a9c2a616d54dcbefb
PRIYAM PATEL <patelpriyam203@gmail.com>
2017-12-05 10:51:43 -0500

    Checkpoint 4 rails kele mentoravailability: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -5,8 +5,12 @@ require 'json'
 class Kele
   include HTTParty
 
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
+
   def initialize(email, password)
-    response = self.class.post("#{api_url}/sessions", body: { "email": email, "password": password })
+    response = self.class.post(api_url("sessions"), body: { "email": email, "password": password })
     if response.code == 404
       puts "Sorry, Invalid email or password"
     end
@@ -14,13 +18,13 @@ class Kele
   end
 
   def get_me
-    response = self.class.get("#{api_url}/('users/me')", headers: { "authorization" => @auth_token })
+    response = self.class.get(api_url('users/me'), headers: { "authorization" => @auth_token })
     @user_data = JSON.parse(response.body)
   end
 
-  private
-
-  def api_url
-    "https://www.bloc.io/api/v1"
+  def get_mentor_availability(mentor_id)
+    response = self.class.get(api_url("mentors/#{mentor_id}/student_availability"), headers: { "authorization" => @auth_token })
+    @mentor_availability = JSON.parse(response.body)
   end
+
 end
