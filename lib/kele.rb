8d15b7c974159f4408e660be279ec76ea5a9ed21
PRIYAM PATEL <patelpriyam203@gmail.com>
2017-12-07 22:22:37 -0500

    Checkpoint 5 rails kele roadmapsandcheckpoints: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -27,4 +27,14 @@ class Kele
     @mentor_availability = JSON.parse(response.body)
   end
 
+  def get_roadmap(roadmap_id)
+    response = self.class.get(api_url("roadmaps/#{roadmap_id}"), headers: { "authorization" => @auth_token })
+    @roadmap_data = JSON.parse(response.body)
+  end
+
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+    @checkpoint_data = JSON.parse(response.body)
+  end
+
 end
