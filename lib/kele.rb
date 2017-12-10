4bdeeed9f6a1289ba0bf4ee090367e0f1c0d0e03
PRIYAM PATEL <patelpriyam203@gmail.com>
2017-12-09 21:52:14 -0500

    Checkpoint 6 rails kele messaging: Completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -37,4 +37,26 @@ class Kele
     @checkpoint_data = JSON.parse(response.body)
   end
 
+  def get_messages(page_num=nil)
+    if page_num == nil
+      response = self.class.get(api_url("message_threads"), headers: { "authorization" => @auth_token })
+    else
+      response = self.class.get(api_url("message_threads?page=#{page_num}"), headers: { "authorization" => @auth_token })
+    end
+    @messages = JSON.parse(response.body)
+  end
+
+  def create_message(user_id, recipient_id, token, subject, message)
+    response = self.class.post(api_url("messages"),
+      body: {
+        "user_id": user_id,
+        "recipient_id": recipient_id,
+        "token": token,
+        "subject": subject,
+        "stripped_text": message
+        },
+        headers: {"authorization" => @auth_token})
+    puts response
+  end
+
 end
